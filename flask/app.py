a2dde1be55ae0812c3048999e6cb187773b775f4
Stefano <stefano.brozzi@gmail.com>
2021-09-20 18:31:15 +0200

    iniziale

---
+++ flask/app.py
@@ -0,0 +1,77 @@
+"""
+https://towardsdatascience.com/web-visualization-with-plotly-and-flask-3660abf9c946
+"""
+from flask import Flask, render_template
+import pandas as pd
+import json
+import plotly
+import plotly.express as px
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/chart1')
+def chart1():
+    df = pd.DataFrame({
+        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+        "Amount": [4, 1, 2, 2, 4, 5],
+        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+    })
+
+    fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    header="Fruit in North America"
+    description = """
+    A academic study of the number of apples, oranges and bananas in the cities of
+    San Francisco and Montreal would probably not come up with this chart.
+    """
+    return render_template('notdash2.html', graphJSON=graphJSON, header=header,description=description)
+
+@app.route('/chart2')
+def chart2():
+    df = pd.DataFrame({
+        "Vegetables": ["Lettuce", "Cauliflower", "Carrots", "Lettuce", "Cauliflower", "Carrots"],
+        "Amount": [10, 15, 8, 5, 14, 25],
+        "City": ["London", "London", "London", "Madrid", "Madrid", "Madrid"]
+    })
+
+    fig = px.bar(df, x="Vegetables", y="Amount", color="City", barmode="stack")
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    header="Vegetables in Europe"
+    description = """
+    The rumor that vegetarians are having a hard time in London and Madrid can probably not be
+    explained by this chart.
+    """
+    return render_template('notdash2.html', graphJSON=graphJSON, header=header,description=description)
+
+@app.route('/chart3')
+def chart3():
+	
+    gapminder_df = px.data.gapminder()
+    #gapminder_df
+
+    fig = px.scatter(data_frame=gapminder_df,
+          x='gdpPercap',
+          y='lifeExp',
+          size='pop',
+          color='continent',
+          title='Life Span',
+          labels={'gdpPercap': 'Wealth',
+                 'lifeExp': 'Life Span'},
+          log_x=True,
+          range_y=[25,95],
+          hover_name='country',
+          animation_frame='year',
+          height=600,size_max=100)
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    header="Wealth and Lifespan"
+    description = """
+    prova1
+    """
+    return render_template('notdash2.html', graphJSON=graphJSON, header=header,description=description)
